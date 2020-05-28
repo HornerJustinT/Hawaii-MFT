a14e5cdc8bbce3b432ef3d9a116636070f67d7f9
Denis Woldemariam <damewold@gmail.com>
2020-05-28 16:19:48 -0500

    added ADD_MEMBER dispatch with all the inputs

--- src/components/ProfileCreate/ProfileCreate.js
+++ src/components/ProfileCreate/ProfileCreate.js
@@ -8,7 +8,35 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 
 
 class ProfileCreate extends Component {
-    
+    //create a state 
+    state = {
+    prefix:'',
+	first_name:'',
+	last_name:'',
+    age:'',
+    hiamft_member_account_info:'',
+	language_id:''
+    }
+
+    //take in the information from the input
+    handleInputChangeFor = propertyName => (event) =>{
+        this.setState({
+          [propertyName]:event.target.value
+        })
+      } 
+    //upload all the inputs into the members table
+    addMembers = (event) =>{
+     event.preventDefault();
+     this.props.dispatch({
+         type:'ADD_MEMBER',
+         payload:{
+            prefix:'',
+            first_name:'',
+            last_name:'',
+            age:'',
+         }
+     })
+    }
     
      handleNext = (event) => {
         event.preventDefault ()
@@ -24,15 +52,27 @@ class ProfileCreate extends Component {
 
         <ProgressBar now={25} />
 
-         <form>
+         <form onSubmit={this.addMembers}>
          <h3>Basic Info</h3>
          <label>Prefix</label>
          <br/>
-         <input/><br/>
+         <input type="text"
+                  name="prefix"
+                  value={this.state.prefix}
+                  onChange={this.handleInputChangeFor("prefix")}/><br/>
          <label>First Name</label>
-         <br/><input/><br/>
-         <label>Last Name</label><br/><input/><br/>
-         <label>Age</label><br/><input/>
+         <br/><input type="text"
+                  name="first_name"
+                  value={this.state.first_name}
+                  onChange={this.handleInputChangeFor("first_name")}/><br/>
+         <label>Last Name</label><br/><inpu type="text"
+                  name="last_name"
+                  value={this.state.last_name}
+                  onChange={this.handleInputChangeFor("last_name")}t/><br/>
+         <label>Age</label><br/><input type="text"
+                  name="age"
+                  value={this.state.age}
+                  onChange={this.handleInputChangeFor("age")}/>
          <label>Language Spoken</label><br/><select><option value='' defaultValue='Select a language'>Select a language</option></select>
         <br/>
         <br/>
@@ -41,15 +81,14 @@ class ProfileCreate extends Component {
         <br/>
         <label>About You</label>
         <br/>
-        <textarea></textarea>
+        <textarea type="text"
+                  name="hiamft_member_account_info"
+                  value={this.state.hiamft_member_account_info}
+                  onChange={this.handleInputChangeFor("hiamft_member_account_info")}/>
         <br/>
         <button>Save</button>
-        <button onClick={this.handleNext}>Save and Next Page</button>
          </form>
-        
-
-
-
+         <button onClick={this.handleNext}>Save and Next Page</button>
             </div>
             </>
         )
