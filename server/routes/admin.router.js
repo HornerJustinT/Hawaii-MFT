c77a61cbb14a958587b8545e30f26bcaa3e8898d
Mark Spannbauer <msmarkieer@gmail.com>
2020-06-01 11:46:51 -0500

    added search by name in admin.router

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -2,9 +2,6 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
 router.get('/', async(req, res) => {
     const connection = await pool.connect();
     try {
@@ -26,6 +23,29 @@ router.get('/', async(req, res) => {
       } finally {
         connection.release();
       }
+});
+
+router.get("/name/:name", async (req, res) => {
+  const connection = await pool.connect();
+  try {
+    let query = `SELECT m.id, m.first_name, m.last_name,
+            array_agg(DISTINCT license_type_pivot.license_number) AS license_number
+
+            FROM members m
+            
+            JOIN license_type_pivot ON license_type_pivot.member_id = m.id
+
+            WHERE LOWER(m.first_name) LIKE $1 OR LOWER(m.last_name) LIKE $1
+		    GROUP BY m.id, m.first_name, m.last_name`;
+    const members = await connection.query(query, ['%' + req.params.name.toLowerCase() + '%']);
+
+    res.send(members.rows);
+  } catch (error) {
+    console.log(`Error Selecting members (admin)`, error);
+    res.sendStatus(500);
+  } finally {
+    connection.release();
+  }
 });
 
 
