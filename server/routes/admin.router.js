42c6aa5479c3c8d5182a85664c3a3f765fd91c6d
Mark Spannbauer <msmarkieer@gmail.com>
2020-06-01 11:59:43 -0500

    added search by id in the admin.router

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -12,7 +12,7 @@ router.get('/', async(req, res) => {
             
             JOIN license_type_pivot ON license_type_pivot.member_id = m.id
     
-		    GROUP BY m.id, m.first_name, m.last_name`;
+		    GROUP BY m.id, m.first_name, m.last_name;`;
         const members = await connection.query(query);
 
         res.send(members.rows)
@@ -36,12 +36,35 @@ router.get("/name/:name", async (req, res) => {
             JOIN license_type_pivot ON license_type_pivot.member_id = m.id
 
             WHERE LOWER(m.first_name) LIKE $1 OR LOWER(m.last_name) LIKE $1
-		    GROUP BY m.id, m.first_name, m.last_name`;
+		    GROUP BY m.id, m.first_name, m.last_name;`;
     const members = await connection.query(query, ['%' + req.params.name.toLowerCase() + '%']);
 
     res.send(members.rows);
   } catch (error) {
-    console.log(`Error Selecting members (admin)`, error);
+    console.log(`Error Selecting members by name (admin)`, error);
+    res.sendStatus(500);
+  } finally {
+    connection.release();
+  }
+});
+
+router.get("/id/:id", async (req, res) => {
+  const connection = await pool.connect();
+  try {
+    let query = `SELECT m.id, m.first_name, m.last_name,
+            array_agg(DISTINCT license_type_pivot.license_number) AS license_number
+
+            FROM members m
+            
+            JOIN license_type_pivot ON license_type_pivot.member_id = m.id
+
+            WHERE CAST(m.id AS VARCHAR) LIKE $1
+		    GROUP BY m.id, m.first_name, m.last_name;`;
+    const members = await connection.query(query, ["%" + req.params.id + '%']);
+
+    res.send(members.rows);
+  } catch (error) {
+    console.log(`Error Selecting members by id (admin)`, error);
     res.sendStatus(500);
   } finally {
     connection.release();
