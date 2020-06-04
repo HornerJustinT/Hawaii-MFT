ef8e1c3db0986dc46bf966be0a5d7d5b8d36c100
HornerJustinT <Horner.Justin.T@gmail.com>
2020-06-04 10:38:43 -0500

    basic profile view data added

--- src/components/ProfileView/ProfileView.js
+++ src/components/ProfileView/ProfileView.js
@@ -11,11 +11,11 @@ import {
   google,
 } from "google-maps-react";
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
-
 const mapStyles = {
   width: "250px",
   height: "250px",
 };
+
 class ProfileView extends Component {
   state = {
     lat: 0,
@@ -27,7 +27,26 @@ class ProfileView extends Component {
       payload: this.props.match.params,
     });
   }
-
+  telehealth=(doesTelehealth)=>{
+    if(doesTelehealth){
+      return <p>Yes I do provide telehealth</p>
+    }
+    else{
+      return <p>No, I do not provide telehealth at this time</p>
+    }
+  }
+  credentials=(credentials)=>{
+    if(credentials){
+      return<ul>{this.props.profile[0].credentials.map((credentials,key) =>
+        <p key={key}>{credentials}</p>)}
+        </ul>
+    }
+  }
+  website=(website)=>{
+    if(website){
+      return <p>{this.props.profile[0].website}</p>
+    }
+  }
   setMAP = () => { // function that sets the map latitudes only when it was unchanged to stop infinete loop. I do not know how to set up async for when the dispatch is done.
     if (this.state.lat == 0 && this.state.lng ==0 ) {
       console.log(this.props.profile[0].address[0]);
@@ -74,23 +93,13 @@ class ProfileView extends Component {
                 <button className="backSearch">Back to search Results</button>
                 <div className="bio-title">
                   <h1>
-                    {this.props.profile.first_name}{" "}
-                    {this.props.profile.last_name}
+                    {this.props.profile[0].first_name}{" "}
+                    {this.props.profile[0].last_name}
                   </h1>
-                  <h3>Counselor, Ma, ATR, LPCC, LAMFT</h3>
+                  {this.credentials()}
                 </div>
                 <div className="bio">
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  <p>{this.props.profile[0].hiamft_member_account_info}
                   </p>
                 </div>
               </div>
@@ -105,45 +114,28 @@ class ProfileView extends Component {
                 </div>
               </div>
               <div className="treatment">
-                <h2>treatments & Approaches</h2>
+                <h2>Treatments & Approaches</h2>
                 <div className="ts">
-                  <ul>
-                    <p>Attachment-based</p>
-                    <p>Dialectical</p>
-                    <p>Family/Maritial</p>
-                    <p>Play Theraphy</p>
-                  </ul>
+                <ul>{this.props.profile[0].treatment_preferences.map((treatment_preferences,key) =>
+                <p key={key}>{treatment_preferences}</p>)}
+                </ul>
                 </div>
                 <div className="approaches">
-                  <ul>
-                    <p>Cogniotive Behaviorial</p>
-                    <p>Emotionally Focused</p>
-                    <p>Narrative</p>
-                    <p>Relational</p>
-                  </ul>
+                  {/* do we have anything for approaches? */}
                 </div>
               </div>
               <div>
                 <h2>Specialities</h2>
-                <ul>
-                  <p>Anxiety</p>
-                  <p>Depression</p>
-                  <p>Trauma & PTSD</p>
-                  <p>Addiction</p>
+                <ul>{this.props.profile[0].specialty.map((specialty,key) =>
+                <p key={key}>{specialty}</p>)}
                 </ul>
               </div>
               <div className="insurance">
                 <h2>Insurance Taken</h2>
                 <div className="box1">
-                  <p>Hawaii Medical</p>
-                  <p>Multiplan</p>
-                  <p>TRICARE</p>
-                  <p>University Health Alliance</p>
-                </div>
-                <div className="box2">
-                  <p>Services Association</p>
-                  <p>Optum</p>
-                  <p>UnitedHealthCare</p>
+                <ul>{this.props.profile[0].ages_served.map((age,key) =>
+                <p key={key}>{age}</p>)}
+                </ul>
                 </div>
               </div>
               <div>
@@ -152,42 +144,39 @@ class ProfileView extends Component {
               </div>
               <div className="telehealth">
                 <h2>Telehealth</h2>
-                <p>Yes, I am providing telehealth</p>
+                {this.telehealth()}
               </div>
               <div className="clientFocus">
                 <h2>Client Focus</h2>
                 <div className="clientAge">
                   <h3>Age</h3>
-                  <p>Any</p>
+                  <ul>{this.props.profile[0].ages_served.map((age,key) =>
+                <p key={key}>{age}</p>)}
+                </ul>
                 </div>
                 <div className="clientDemographics">
                   <h3>Demographics</h3>
-                  <ul>
-                    <p>Any</p>
-                    <p>Gay/Lesbian/Bisexual/Transgender</p>
-                    <p>Women/Feminist</p>
-                  </ul>
+                  <ul>{this.props.profile[0].client_focus.map((age,key) =>
+                <p key={key}>{age}</p>)}
+                </ul>
                 </div>
               </div>
               <div>
                 <h2>Languages Spoken</h2>
-                <ul>
-                  <p>English</p>
-                  <p>Tagalog</p>
+                <ul>{this.props.profile[0].languages.map((language,key) =>
+                <p key={key}>{language}</p>)}
                 </ul>
               </div>
               <div>
                 <h2>Session Formats</h2>
-                <ul>
-                  <p>Individuals</p>
-                  <p>Coouples</p>
-                  <p>Families</p>
+                <ul>{this.props.profile[0].session_format.map((session_format,key) =>
+                <p key={key}>{session_format}</p>)}
                 </ul>
               </div>
             </div>
             <div className="rightside">
               <div>
-                <h1>Kailua, O'ahu</h1>
+                <h1>{this.props.profile[0].city}, {this.props.profile[0].island}</h1>
                 <button>Send Referral Email</button>
               </div>
               <div>
@@ -196,11 +185,14 @@ class ProfileView extends Component {
               <div>
                 <h2>Contact</h2>
                 <ul>
-                  <p>808-123-4567</p>
-                  <p>jane@familytheraphy.com</p>
-                  <p>familytheraphy.com</p>
-                  <p>123 Dreamhouse Lane</p>
-                  <p>Kailua, Hawaii</p>
+                  <p>{this.props.profile[0].phone[0]}</p>
+                  {/* Since there will only be one phone and no non business numbersI think we can simply just call the first one */}
+                  <p>{this.props.profile[0].email[0]}</p>
+                  {/* Same with email */}
+                  {this.website()}
+                  {/* chceks to see if website is there adds if there is */}
+                  <p>{this.props.profile[0].address}</p>
+                  <p>{this.props.profile[0].city}, Hawaii</p>
                 </ul>
                 <div className="map">
                   <Map
