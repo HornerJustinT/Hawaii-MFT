cc773ff01b48e7b327baddda0addc3de5a783732
Mark Spannbauer <msmarkieer@gmail.com>
2020-06-05 13:22:04 -0500

    updating small things

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../SearchBar/SearchBar'
-import EmailModal from '../EmailModal/EmailModal';
 
 
 class HomePage extends Component{
     render(){
         return(
             <>
-            <div>
-                <div>
-                    <EmailModal/>
-                </div>
                 <SearchBar />
-            </div>
             </>
         );
     }
