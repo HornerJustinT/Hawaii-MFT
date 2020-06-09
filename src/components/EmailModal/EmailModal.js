3b2ea2aaa881a792056952f7d820f52f2f2e33f1
HornerJustinT <Horner.Justin.T@gmail.com>
2020-06-09 01:39:34 -0500

    fixed email modal defaults

--- src/components/EmailModal/EmailModal.js
+++ src/components/EmailModal/EmailModal.js
@@ -1,64 +1,61 @@
-import React, {  useState  } from 'react';
+import React, { useState } from "react";
 
 //React-bootstrap imports
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { connect } from "react-redux";
 
+function EmailModal(props) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  console.log(props);
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Send Referral Email
+      </Button>
 
-function EmailModal() {
-    const [show, setShow] = useState(false);
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Send Referral Email for Jane Rain</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Recipient's Email Address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Send Referral Email
+            <Form.Group controlId="formBriefMessage">
+              <Form.Label>Brief Message</Form.Label>
+              <Form.Control
+                type="input"
+                as="textarea"
+                rows="10"
+                placeholder={`Hello, 
+${props.profile.first_name} ${props.profile.last_name} is a colleague of mine I think she'd be a great fit for you! Below is her contact info.
+Best wishes!`}
+              />
+              <Form.Control
+                rows="5"
+                plaintext
+                readOnly
+                defaultValue={`${props.profile.website} ${props.profile.phone} ${props.profile.address}
+                             `}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={handleClose}>
+              Send Email
             </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Send Referral Email for Jane Rain</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Recipient's Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-
-                        <Form.Group controlId="formBriefMessage">
-                            <Form.Label>Brief Message</Form.Label>
-                            <Form.Control type="input" 
-                            as="textarea" rows="10"
-                            placeholder="Hi Emily,
-                            Jane Rain is a colleague of mine who is also on O'ahu and I think she'd be a great fit for you. 
-                            Below is her contact info.
-                            Best wishes!"
-                            />
-                        <Form.Control 
-                            rows="5"
-                            plaintext 
-                            readOnly 
-                            defaultValue="Jane Rain
-                                jane@familytherapy.com
-                                806-123-4567
-
-                                123 Dreamhouse Lane
-                                Kailua, Hawaii
-                            " />
-                        </Form.Group>
-                        <Button variant="primary" onClick={handleClose}>
-                            Send Email
-                        </Button>
-                    </Form>
-                </Modal.Body>
-            </Modal>
-        </>
-    );
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
-
-export default EmailModal;
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+export default connect(mapStateToProps)(EmailModal);
