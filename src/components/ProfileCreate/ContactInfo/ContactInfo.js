989b79185abd72720dfb59f5849aeccae824d553
Denis Woldemariam <damewold@gmail.com>
2020-07-08 11:29:34 -0500

    got rid of the toggele button and student contact info inputs

--- src/components/ProfileCreate/ContactInfo/ContactInfo.js
+++ src/components/ProfileCreate/ContactInfo/ContactInfo.js
@@ -69,25 +69,7 @@ componentDidUpdate = () => {
       } 
       
 
-      validateStudent = () =>{
-        let personalEmailError ='';
-        let formIsValid = true;
-
-        if(this.state.personal_email === ''){
-          formIsValid=false;
-          personalEmailError = 'Personal Email is required'
-        }else if(!this.state.personal_email.includes('@') ){
-            formIsValid=false;
-            personalEmailError = "Personal Email is not valid"
-        }
-
-        if(personalEmailError){
-          this.setState({personalEmailError})
-        }else{
-          return true;
-        }
-
-      }
+     
 
       validate = () => {
           let islandError = '';
@@ -252,111 +234,14 @@ studentContactInfo = (e) =>{
      }
    
     render (){
-      let itemToRender;
 
-       if(this.state.showStudentProfile){
-        itemToRender = 
-        <div>
-             <div>
-                          <Prompt
-        when={this.state.shouldBlockNavigation}
-        message='You have unsaved changes, are you sure you want to leave?'
-      />
-
-        </div>
-            <Form onSubmit={this.studentContactInfo}>
-             <br/>
-             <Form.Check
-              type="switch"
-              id="custom-switch"
-              label="Are you a Student?"
-              className="switch"
-              onChange={this.handleStudent}
-            />
-                <br/>
-             <Form.Label>Island</Form.Label>
-             <br/>
-             <Form.Control
-                 as="select" onChange={this.handleInputChangeFor("island_id")}>
-             {this.props.islands &&
-                   
-                   <>
-                   <option defaultValue='Select your Island'>Select your Island</option>
-                   {this.props.islands.map(island =>
-                    <option value={island.island_id}
-        
-                  key={island.island_id}>{island.title}</option>
-                    )}
-                   </>
-                   } 
-             </Form.Control>
-             <h4 className="error">{this.state.islandError}</h4>
-             <br/>
-             <br/>
-             <Form.Label>Zip Code</Form.Label>
-             <br/>
-             <Form.Control type="number"
-                  name="zip_code"
-                  placeholder='Please fill your zip code'
-                  value={this.state.zip_code}
-                  onChange={this.handleInputChangeFor("zip_code")}/>
-                  <h4 className="error">{this.state.zipCodeError}</h4>
-             <br/>
-             <Form.Label>Phone Number - Personal</Form.Label>
-             <br/>
-             <Form.Control type="number"
-                  name="personal_number"
-                  placeholder='Please fill in your personal phone number'
-                  value={this.state.personal_number}
-                  onChange={this.handleInputChangeFor("personal_number")}/>
-                  <h4 className="error">{this.state.personalNumberError}</h4>
-             <br/>
-             <Form.Label>Email Address - Personal*</Form.Label>
-             <br/>
-             <Form.Control type="text"
-                  name="personal_email"
-                  placeholder='Please fill in your personal email address'
-                  value={this.state.personal_email}
-                  onChange={this.handleInputChangeFor("personal_email")}/>
-                  <h4 className="error">{this.state.personalEmailError}</h4>
-             <br/>
-             <Form.Label>Website</Form.Label>
-             <br/>
-             <Form.Control type="text"
-                  name="website"
-                  placeholder='Please fill in your website'
-                  value={this.state.website}
-                  onChange={this.handleInputChangeFor("website")}/>
-                  <h4 className="error">{this.state.websiteError}</h4>
-             <br/>
-             <Form.Label>Address - Home</Form.Label>
-             <br/>
-             <Form.Control type="text"
-                  name="address_home"
-                  placeholder='Please fill in your home address'
-                  value={this.state.address_home}
-                  onChange={this.handleInputChangeFor("address_home")}/>
-                  <h4 className="error">{this.state.addressHomeError}</h4>
-             <br/>
-             <Form.Label>City</Form.Label>
-             <br/>
-             <Form.Control type="text"
-                  name="city"
-                  placeholder='Please fill in your city name'
-                  value={this.state.city}
-                  onChange={this.handleInputChangeFor("city")}/>
-                  <h4 className="error">{this.state.cityError}</h4>
-             <br/>
-             <br/>
-            
-           <Button type="submit">Next</Button>
-            </Form>
-            
-        </div>
-       }else{
-         itemToRender = 
-         <div>
-              <div>
+        return(
+            <>
+            <div className='container'>
+            <header><h1 className="text-center">Contact Info</h1></header>
+            <ProgressBar now={50} />
+              
+            <div>
                           <Prompt
         when={this.state.shouldBlockNavigation}
         message='You have unsaved changes, are you sure you want to leave?'
@@ -364,14 +249,6 @@ studentContactInfo = (e) =>{
 
         </div>
                    <Form onSubmit={this.addContactInfo}>
-             <br/>
-             <Form.Check
-              type="switch"
-              id="custom-switch"
-              label="Are you a Student?"
-              className="switch"
-              onChange={this.handleStudent}
-            />
                 <br/>
              <label>Island*</label>
              <br/>
@@ -492,17 +369,10 @@ studentContactInfo = (e) =>{
                   <h4 className="error">{this.state.businessCityError}</h4>
              <br/>
             
-           <Button type="submit">Next</Button>
+             <div  className="next-button">
+                  <Button type="submit">Next</Button>
+                  </div>
             </Form>
-         </div>
-       }
-      
-        return(
-            <>
-            <div className='container'>
-            <header><h1 className="text-center">Contact Info</h1></header>
-            <ProgressBar now={50} />
-              {itemToRender}
             </div>
            
 
