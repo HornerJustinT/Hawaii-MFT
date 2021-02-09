34a628e5f56374e22790ba696a4169e0d0afc3c0
Kristen Stoeckeler <kstoeckeler@gmail.com>
2021-02-09 17:47:42 -0600

    fixed  timeout

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -24,7 +24,7 @@ if (process.env.DATABASE_URL) {
     database: params.pathname.split('/')[1],
     ssl: true, // heroku requires ssl to be true
     max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    idleTimeoutMillis: 29000, // how long a client is allowed to remain idle before being closed
   };
 } else {
   config = {
